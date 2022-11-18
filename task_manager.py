cc2451e2c95ef33d5b4fda23d1701384a086f00a
Epimethic <85319175+Epimethic@users.noreply.github.com>
2022-11-18 00:49:05 +0000

    Update task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -51,18 +51,19 @@ while True:
         print("You entered the incorrect username and password.")
 
  # while loop that iterates the menu until the user logs out by typing in 'e'
+#
 while True:
     admin = False
     if input_user == "admin" and input_password == "adm1n":
         admin = True
     if admin:
         menu = input("Select one of the following Options below:\n"
-                     "s - Statistics\n"
-                     "r - Registering a user\n"
-                     "a - Adding a task\n"
-                     "va - View all tasks\n"
-                     "vm - View my task\n"
-                     "e - Exit\n".lower())
+                    "s - Statistics\n"
+                    "r - Registering a user\n"
+                    "a - Adding a task\n"
+                    "va - View all tasks\n"
+                    "vm - View my task\n"
+                    "e - Exit\n".lower())
 
         if menu.lower() == "s":
             with open("user.txt", "r") as f:
@@ -70,46 +71,46 @@ while True:
                 print(f"________________________________________\nThere are a total of {x} registered users.\n_________"
                 f"_______________________________")
 
-            with open("tasks.txt", "r") as f:
-                y = len(f.readlines())
-                print(f"________________________________________\nThere are a total of {y} registered tasks.\n_________"
+        with open("tasks.txt", "r") as f:
+            y = len(f.readlines())
+            print(f"________________________________________\nThere are a total of {y} registered tasks.\n_________"
                           f"_______________________________")
 
-            # if user inputs 'r' to register a new user they will be asked to input the new user and password
+        # if user inputs 'r' to register a new user they will be asked to input the new user and password
         if menu.lower() == "r":
             new_user_name = input("Please enter the new username you would like to register:  ")
             new_user_password = input("Please create a password for the new user you would like to register: ")
             password_confirmation = input("Please re-enter your password to confirm: ")
 
-             # nested if statement activates if the new password and confirmed password match
+            #   nested if statement activates if the new password and confirmed password match
             if password_confirmation == new_user_password:
-                 # an empty user list is created to assign new users to later
+                # an empty user list is created to assign new users to later
                 new_user_list = []
                 # new_user concatenates new_suer_name and _new_user_password
                 new_user = f"{new_user_name}, {new_user_password}"
-                 # append command to add a new user to the new user list
+                # append command to add a new user to the new user list
                 new_user_list.append(new_user)
 
-                # with open txt file set to a+ to append to the document without deleting any data
-                with open("user.txt", "a+") as f:
-                    # for loop iterates for each appended new user and writes this into the txt file
-                    for new_user in new_user_list:
-                        f.write(f"{new_user_name}, {new_user_password}\n")
+             # with open txt file set to a+ to append to the document without deleting any data
+            with open("user.txt", "a+") as f:
+                # for loop iterates for each appended new user and writes this into the txt file
+                for new_user in new_user_list:
+                    f.write(f"{new_user_name}, {new_user_password}\n")
 
-        elif input_user != "admin" and input_user != "adm1n":
-            admin = False
-            if admin == False:
-                 menu = input("Select one of the following Options below:\n"
-                 # r - Registering a user
-                "a - Adding a task\n"
-                "va - View all tasks\n"
-                "vm - View my task\n"
-                 "e - Exit\n".lower())
+    elif input_user != "admin" and input_user != "adm1n":
+        admin = False
+        if admin == False:
+            menu = input("Select one of the following Options below:\n"
+             # r - Registering a user
+            "a - Adding a task\n"
+            "va - View all tasks\n"
+            "vm - View my task\n"
+            "e - Exit\n".lower())
 
 
 
     if menu == 'a':
-        # variables needed to register a new task
+    # variables needed to register a new task
         new_task_username = input("Enter the username of whom the task will be assigned to: ")
         new_task_title = input("Enter a title for the task: ")
         task_description = input("Please enter a description of the task: ")
@@ -120,7 +121,7 @@ while True:
         # all of the above variables assigned to the new_task_information variable ready to be added to new task list
         new_task_information_list = []
         new_task_information = f"{new_task_username}, {new_task_title}, {task_description}, {today[0:10]}, " \
-                               f"{task_due_date}, {task_complete_no}"
+                                f"{task_due_date}, {task_complete_no}"
 
         # append command to add new_task_information to a list
         new_task_information_list.append(new_task_information)
@@ -146,13 +147,13 @@ while True:
                 # variables assigned in a user friendly way
                 read_task_list = []
                 read_task = "-------------------------------------------------------------------------\n" \
-                                       f"Assigned to: \t \t {name_assigned}\n" \
-                                       f"Task Name: \t \t     {task_title}\n" \
-                                       f"Task Description: \t {task_description}\n" \
+                                        f"Assigned to: \t \t {name_assigned}\n" \
+                                        f"Task Name: \t \t     {task_title}\n" \
+                                        f"Task Description: \t {task_description}\n" \
                                         f"Date Assigned: \t \t {date_issued}\n" \
                                         f"Due Date: \t \t     {date_due}\n" \
-                                       f"Task Complete: \t \t {task_complete_no}\n" \
-                                       f"------------------------------------------------------------------------\n"
+                                        f"Task Complete: \t \t {task_complete_no}\n" \
+                                        f"------------------------------------------------------------------------\n"
                 # read task variable added to the read_task_list
                 read_task_list.append(read_task)
                 # for loop prints the information in the read_task variable for each iteration, which shows all tasks
@@ -161,7 +162,7 @@ while True:
 
     elif menu == 'vm':
         # with open task set as r to read
-        with open('tasks.txt', 'r') as f:
+         with open('tasks.txt', 'r') as f:
             # for loop reads, strips and splits every line and then uses indexing to create variables with specific info
             for line in f.readlines():
                 temp = line.strip().split(", ")
@@ -182,10 +183,10 @@ while True:
                             f"Due Date: \t \t     {date_due}\n" \
                             f"Task Complete: \t \t {task_complete_no}\n" \
                             f"------------------------------------------------------------------------\n"
-               # append command is used to add each read task to a list
+                   # append command is used to add each read task to a list
                 read_task_list.append(read_task)
 
-                # if statement checks if the username used to log in matches the username tasks are assigned to
+                    # if statement checks if the username used to log in matches the username tasks are assigned to
                 if input_user == name_assigned:
                     # for loop prints every read_task variable for each task that is assigned to the logged in user
                     for read_task in read_task_list:
